b0b3888dc69f028b7767a387157a3a5f8c15bd97
tenney <Sandy970701@Gmail.com>
2024-03-14 14:26:34 +0800

    2024-03-14 14-26-34

--- gitp_shell_floder.sh
+++ gitp_shell_floder.sh
@@ -1,4 +1,4 @@
-# #!/bin/zsh
+#!/bin/zsh
 # Set environment variables
 # 检查 env.txt 文件是否存在
 if [ -f "${HOME}/env.txt" ]; then
@@ -11,6 +11,7 @@ else
   # 运行其他命令
   echo "env.txt 文件不存在"
 fi
+
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo $SHELL_FOLDER
 cd $SHELL_FOLDER
